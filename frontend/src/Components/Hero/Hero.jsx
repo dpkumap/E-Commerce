b8ec4c580e68c0e40e8317cf4261dd96d13ff7f9
dpkumap <deepakumap4141@gmail.com>
2024-04-09 18:16:17 +0530

    heroSectionDone

--- frontend/src/Components/Hero/Hero.jsx
+++ frontend/src/Components/Hero/Hero.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import './Hero.css'
 import hand_icon from '../Assets/hand_icon.png'
-import arrow_icon from './Assets/arrow.png'
-import hero_image from './Assets/hero_image.png'
+import arrow_icon from '../Assets/arrow.png'
+import hero_image from '../Assets/hero_image.png'
 
 const Hero=()=>{
 
@@ -12,19 +12,23 @@ const Hero=()=>{
 
             <div className="hero-left">
                 <h2>NEW ARRIVALS ONLY</h2>
-                <div className="hand-hand-icon">
+                <div>
+                <div className="hero-hand-icon">
                     <p>new</p>
                     <img src={hand_icon} alt="hand icon" />
                 </div>
                 <p>Collections</p>
-                <p>For every one</p>
-            </div>
+                <p>For everyone</p>
+                </div>
+            
             <div className="hero-latest-btn">
                 <div>Latest Collections</div>
                 <img src={arrow_icon} alt="arrow" />
             </div>
+            </div>
             <div className="hero-right">
 
+                <img src={hero_image} alt="heroimg" />
             </div>
 
         </div>
