505b06f17a5c54b05f936df513cda05c8e250520
dpkumap <deepakumap4141@gmail.com>
2024-04-11 14:43:36 +0530

    offers component added

--- frontend/src/Components/Popular/Popular.jsx
+++ frontend/src/Components/Popular/Popular.jsx
@@ -13,7 +13,11 @@ const Popular=()=>{
 
                 {data_product.map((item,i)=>{
 
-                    return <Item key={i} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price}/>
+return (
+    <div className="item-container" key={i}>
+        <Item id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price}/>
+    </div>
+);
                 })}
             </div>
         </div>
