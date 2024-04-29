e5c138ec94d928c36ac9d4cd34e78068e39af877
dpkumap <deepakumap4141@gmail.com>
2024-04-29 14:00:58 +0530

    changing url

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,7 +11,18 @@ import Footer from './Components/Footer/Footer';
 import men_banner from './Components/Assets/banner_mens.png'
 import women_banner from './Components/Assets/banner_women.png'
 import kids_banner from './Components/Assets/banner_kids.png'
+import { useState,useEffect } from 'react';
+
 function App() {
+
+  const [message, setMessage] = useState("");
+
+  // Fetching message from backend on mount
+  useEffect(() => {
+    fetch("https://crownmode-e-commerce.onrender.com")
+      .then((res) => res.json())
+      .then((data) => setMessage(data.message));
+  }, []);
   return (
     <div >
       <BrowserRouter>
