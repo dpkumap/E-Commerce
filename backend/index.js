ae7ebdd4cca426fd84f8cb360f1c8d87a1e815ca
dpkumap <deepakumap4141@gmail.com>
2024-05-01 15:24:13 +0530

    imgurlofadminpanelchanged

--- backend/index.js
+++ backend/index.js
@@ -115,7 +115,7 @@ app.post("/upload",upload.single('product'),(req,res)=>{
 
     res.json({
         success:1,
-        image_url:`http://localhost:${PORT}/images/${req.file.filename}`
+        image_url:`https://crownmode-be.onrender.com/images/${req.file.filename}`
     })
 })
 
