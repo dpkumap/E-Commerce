cf59e113612c5172c6b853d4b4b1ae5dec90bd03
dpkumap <deepakumap4141@gmail.com>
2024-10-27 19:15:03 +0530

    fixbugs

--- frontend/src/Components/ProductDisplay/ProductDisplay.jsx
+++ frontend/src/Components/ProductDisplay/ProductDisplay.jsx
@@ -44,8 +44,8 @@ const ProductDisplay = (props) => {
                     <p>(222)</p>
                 </div>
                 <div className="productdisplay-right-prices">
-                    <div className="productdisplay-right-price-old">${product.old_price}</div>
-                    <div className="productdisplay-right-price-new">${product.new_price}</div>
+                    <div className="productdisplay-right-price-old">₹{product.old_price}</div>
+                    <div className="productdisplay-right-price-new">₹{product.new_price}</div>
                 </div>
                 <div className="productdisplay-right-description">
                     Stay cozy in style with our green hoody, crafted with comfort and durability for everyday wear.
